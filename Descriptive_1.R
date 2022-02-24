20b74bb49a0e2c65420a47f0be6067eb7f407402
Gabriel <catano@uthscsa.edu>
2022-02-23 23:49:45 -0600

    demographics

---
+++ Descriptive_1.R
@@ -0,0 +1,23 @@
+library(tableone)
+library(lubridate)
+library(dplyr)
+
+data_dem_admi <- ADMISSIONS %>%
+  group_by(subject_id) %>%
+  summarize(insurance = first(insurance),
+            admittime = max(admittime),
+            language = first(language),
+            religion = first(religion),
+            marital_status = first(marital_status),
+            ethnicity = first(ethnicity))
+
+
+
+demography <- merge(x=PATIENTS , y=data_dem_admi, by = "subject_id") %>%
+  mutate(age=(dob %--% admittime) %/% years(1),
+         age=if_else(age==300, 90, age)) %>%
+  select(-c(dob, admittime,dod, dod_hosp, dod_ssn))
+
+
+
+CreateTableOne(data = demography)
