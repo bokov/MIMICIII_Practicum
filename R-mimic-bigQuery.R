51fd48206cf749de62a75070089d63db462e24a4
Alex Bokov <bokov@uthscsa.edu>
2022-03-31 17:37:09 -0500

    data(R-mimic-bigQuery.R): Edgar got GCP connections to work, and this is the distillation of those efforts! Parameterized to use config.R and local.config.R to overwrite the GOOGLE_EMAIL variable with your actual email.

---
+++ R-mimic-bigQuery.R
@@ -0,0 +1,25 @@
+# BigQuery - MIMIC-IV - RUN AS JOB (defaults)
+
+library(dplyr)
+library(bigrquery) # bigQ
+library(lubridate)
+
+# provides GOOGLE_EMAIL,PROJECTID, and SQLQUERY00
+source('config.R');
+if(file.exists('local.config.R')) source('local.config.R');
+
+message(as.character(Sys.time()),"Conectando a BigQuery service \n")
+
+options(gargle_oauth_email = GOOGLE_EMAIL)
+
+# fecha actual ----
+hoy <- lubridate::today()
+
+bq_auth(); # This may prompt you for which pre-authorized credentials to use
+tb <- bq_project_query(PROJECTID, SQLQUERY00);
+
+# Ejecutar query & crear dataframe ----
+message(as.character(Sys.time()),"Ejecutando query... \n")
+
+dat1 <- bq_table_download(tb) # ej. max_results = 1000
+save(dat1,file.path('data','cached_data.rdata'));
--- config.R
+++ config.R
@@ -1,2 +1,7 @@
 # This is a file of project-wide settings.
 .projectseed <- 220131;
+GOOGLE_EMAIL <- 'YOURNAME@google.com';
+PROJECTID <- 'mimiciii-6203-2022';
+SQLQUERY00 <- sprintf('SELECT *
+                      FROM `%s.initial_characterization.mp_allpatients`
+                      WHERE age >= 18',PROJECTID);
